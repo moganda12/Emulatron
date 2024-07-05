785022f7b64a203eb1fac885a1d64f20bec5a21f
moganda12 <115957608+moganda12@users.noreply.github.com>
2024-07-05 17:02:25 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <fstream>
+
+#include "./memory.hpp"
+
+using str = std::string;
+
+static constexpr size_t KB = 1'024;
+static constexpr size_t MB = 1'048'576;
+static constexpr size_t GB = 1'073'741'824;
+
+static constexpr int minArgs = 1;
+
+namespace W6502 {
+	struct StatusRegister {
+	    Byte C : 1;
+        Byte Z : 1;
+        Byte I : 1;
+        Byte D : 1;
+	    Byte B : 1;
+	    Byte V : 1;
+	    Byte N : 1;
+    };
+
+	struct CPU {
+        Word IP, SP;
+
+	    Byte ACC, X, Y;
+	    StatusRegister SR;
+    };
+
+}
+
+namespace MegaCPU {
+}
+
+size_t getFileSize(const str& path) {
+	std::ofstream file(path, std::ios::app);
+	return file.tellp();
+}
+
+template <typename T>
+inline void bindump(std::ofstream& file, T value) {
+	void* bindata = &value;
+	file.write((char*)bindata, sizeof(value));
+	return;
+}
+
+template <typename T>
+inline void binup(std::ifstream& file, T& value) {
+	file.read((char*)&value, sizeof(value));
+	return;
+}
+
+int main(int argc, char** argv) {
+	if(argc <= minArgs) {
+		std::cout << "Missing inputs!\n Usage: <path> <romfile>\n";
+		exit(1);
+	}
+
+	str rompath = argv[1];
+
+	Memory<KB*64> memory;
+	{
+		std::ifstream file(argv[1], std::ios::binary);
+		if(!file.is_open()) {
+			std::cout << "It is doom! Failed to open file! Error code 2\n";
+			exit(2);
+		}
+		const size_t size = getFileSize(rompath);
+		if(size != sizeof(memory)) {
+			std::cout << "It is doom! File size is not equal to memory size! Error code 3\n";
+			std::cout << "Would you like to nuke rom (y/n)? ";
+			char c;
+			std::cin >> c;
+			if(c == 'y' || c == 'Y') {
+				std::ofstream nuke(argv[1], std::ios::binary);
+				std::cout << "Nuking rom ☢️ 💣\n";
+				bindump(nuke, memory);
+			    nuke << std::flush;
+				nuke.close();
+				std::cout << "rom nuked ☠️ ☢️ 💥\n";
+			} else if(c == 'n' || c == 'N') {
+				exit(3);
+			}
+		} else {
+			file.seekg(0, std::ios::beg);
+			binup(file, memory);
+		}
+	}
+
+	W6502::CPU cpu6502;
+
+	Register a;
+	a.bl = 10;
+	std::cout << a.content[0] << '\n';
+}
---
+++ memory.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <cstddef>
+
+using Byte = unsigned char;
+using Word = unsigned short;
+using DWord = unsigned int;
+using QWord = size_t;
+
+template <size_t length>
+class Memory {
+private:
+	Byte data[length];
+public:
+	Byte fetch8(size_t address) {
+		return data[address];
+	}
+	unsigned short fetch16(size_t address) {
+		return data[address] | (data[address + 1] << 8);
+	}
+	unsigned int fetch32(size_t address) {
+		return data[address] | (data[address + 1] << 8) | (data[address + 2] << 16) | (data[address + 3] << 24);
+	}
+	size_t fetch64(size_t address) {
+		return data[address] | (data[address + 1] << 8) | (data[address + 2] << 16) | (data[address + 3] << 24) | (data[address + 4] << 32) | (data[address + 5] << 40) | (data[address + 6] << 48) | (data[address + 7] << 56);
+	}
+	void store8(size_t address, Byte value) {
+		data[address] = value;
+	}
+	void store16(size_t address, unsigned short value) {
+		data[address] = value & 0xFF;
+		data[address + 1] = value >> 8;
+	}
+	void store32(size_t address, unsigned int value) {
+		data[address] = value & 0xFF;
+		data[address + 1] = (value >> 8) & 0xFF;
+		data[address + 2] = (value >> 16) & 0xFF;
+		data[address + 3] = (value >> 24) & 0xFF;
+	}
+	void store64(size_t address, size_t value) {
+		data[address] = value & 0xFF;
+		data[address + 1] = (value >> 8) & 0xFF;
+		data[address + 2] = (value >> 16) & 0xFF;
+		data[address + 3] = (value >> 24) & 0xFF;
+		data[address + 4] = (value >> 32) & 0xFF;
+		data[address + 5] = (value >> 40) & 0xFF;
+		data[address + 6] = (value >> 48) & 0xFF;
+		data[address + 7] = (value >> 56) & 0xFF;
+	}
+	size_t getlen() {
+		return length;
+	}
+	Byte* getdata() {
+		return data;
+	}
+};
+
+struct Register {
+    Byte* content;
+    Byte& bl = content[0];
+	Byte& bh = content[1];
+    //Word& wl = ((Word*)content)[0];
+    //Word& wh = ((Word*)content)[1];
+    //DWord& el = ((DWord*)content)[0];
+    //DWord& eh = ((DWord*)content)[1];
+    //QWord& rx = *content;
+};
